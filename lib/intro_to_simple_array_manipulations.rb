a718b07197e1925427ac4465c9ef26053fca080e
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2020-12-26 03:49:36 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+def using_push (array, string)
+  array = ["red", "orange", "yellow", "green", "blue", "indigo"]
+  array.push ("violet")
+end
+
+
+def using_unshift (array, string)
+  array = ["Brooklyn", "Queens", "Manhattan", "Bronx"]
+  array.unshift ("Staten Island")
+end
+
+def using_pop (string)
+  array = ["Africa", "Asia", "Europe", "North America", "South America", "Oceania", "Antarctica"]
+  array.pop
+  
+end
+
+
+def pop_with_args
+  array = ["Great Dane", "Golden Retriever", "Chihuahua", "Shiba Inu"]
+  array.pop(2)
+end
